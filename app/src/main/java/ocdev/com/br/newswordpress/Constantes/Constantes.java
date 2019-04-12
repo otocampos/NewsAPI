bef8a9a7de7a76e078f3a19a513717b40d10a4fc
Oto <90807@pmerj.rj.gov.br>
2019-04-12 13:42:16 -0300

    Initial commit

---
+++ app/src/main/java/ocdev/com/br/newswordpress/Constantes/Constantes.java
@@ -0,0 +1,20 @@
+package ocdev.com.br.newswordpress.Constantes;
+
+public class Constantes {
+
+    public static final String KeyNewsApi = "9f1c23c070554d07a7ca33ee02890591";
+    public static final String CONSTANTE_KEY_API = "apiKey";
+    public static final String BASE_URL = "https://newsapi.org/v2/";
+    public static final String TOP_HEADLINES = "top-headlines/";
+
+
+    public static final  String COUNTRY = "country";
+
+    public static final String COUNTRY_NEWS = "br";
+
+    public static final String[] LISTA_DE_CATEGORIAS = {
+            "business", "entertainment", "general", "health", "science",
+            "sports", "technology"
+    };
+
+}
---
+++ app/src/main/java/ocdev/com/br/newswordpress/Data/Rest/ApiService.java
@@ -0,0 +1,14 @@
+package ocdev.com.br.newswordpress.Data.Rest;
+
+import io.reactivex.Single;
+import ocdev.com.br.newswordpress.Constantes.Constantes;
+import ocdev.com.br.newswordpress.Data.Model.ResponseNews;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiService {
+
+    @GET(Constantes.TOP_HEADLINES)
+    Single<ResponseNews> getNewsByCountry(@Query(Constantes.COUNTRY) String country, @Query(Constantes.CONSTANTE_KEY_API) String keyapi);
+
+}
