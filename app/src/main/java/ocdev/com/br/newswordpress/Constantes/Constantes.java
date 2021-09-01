c4260e6d9e7f19d4c002dbc60b86c4d8d710f6e9
otocampos <otniel41apm@gmail.com>
2021-09-01 09:56:58 -0300

    Update Constantes.java

--- app/src/main/java/ocdev/com/br/newswordpress/Constantes/Constantes.java
+++ app/src/main/java/ocdev/com/br/newswordpress/Constantes/Constantes.java
@@ -2,7 +2,7 @@ package ocdev.com.br.newswordpress.Constantes;
 
 public class Constantes {
 
-    public static final String KeyNewsApi = "9f1c23c070554d07a7ca33ee02890591";
+    public static final String KeyNewsApi = "SUA_API_KEY_AQUI";
     public static final String CONSTANTE_KEY_API = "apiKey";
     public static final String BASE_URL = "https://newsapi.org/v2/";
     public static final String TOP_HEADLINES = "top-headlines/";
